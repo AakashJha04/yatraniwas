1da8790c0ea4ba4009322de3e88af779e4e46063
AakashJha04 <aakashthedeveloper04@gmail.com>
2025-05-15 14:00:52 +0530

    firDmmit

---
+++ src/main/java/com/example/yatraniwas/YatraniwasApplication.java
@@ -0,0 +1,13 @@
+package com.example.yatraniwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatraniwasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatraniwasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yatraniwas/YatraniwasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yatraniwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatraniwasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
